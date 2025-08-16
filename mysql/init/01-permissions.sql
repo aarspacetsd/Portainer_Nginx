67682b702b95dc1912cb702b789b5dac9270ed90
aarspacetsd <emailkamu@example.com>
2025-08-16 18:14:47 +0700

    config nginx

--- mysql/init/01-permissions.sql
+++
@@ -1,6 +0,0 @@
--- Grant additional permissions to application user
-GRANT ALL PRIVILEGES ON *.* TO 'appuser'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-
--- Create additional databases if needed
--- CREATE DATABASE IF NOT EXISTS laravel_test;
